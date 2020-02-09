ab5a5335dc9ebabb8813ace4708dd2396d6188c9
Rustam <rustam.aliev.work@yandex.ru>
2020-02-09 10:54:12 +0300

    The viewsets class has been added.

--- mysite/profiles_api/views.py
+++ mysite/profiles_api/views.py
@@ -1,6 +1,6 @@
 from profiles_api import serializers
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -43,4 +43,17 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete of an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API viewset"""
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+        return Response({'message': 'Hello', 'a_viewset_benefits': a_viewset})
 
