8f1d92a6a0aa4783b6b16cee80cf5b34e5ec668a
Rustam <rustam.aliev.work@yandex.ru>
2020-02-09 10:33:31 +0300

    The put, patch and delete methods implementation has been added to HelloApiView.

--- mysite/profiles_api/views.py
+++ mysite/profiles_api/views.py
@@ -30,3 +30,16 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle full updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete of an object"""
+        return Response({'method': 'DELETE'})
+
